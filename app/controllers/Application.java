561d0f486e966f3dff923ccc055201da5cebeacd
edgar-ach <chirix27@gmail.com>
2015-02-11 12:45:21 -0500

    Seguridad

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -6,7 +6,7 @@ import play.mvc.*;
 import java.util.*;
 
 import models.*;
-
+@With(Secure.class)
 public class Application extends Controller {
 
     public static void index() {
---
+++ app/controllers/Security.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import models.Usuario;
+
+public class Security extends Secure.Security {
+    
+    static boolean authenticate(String username, String password) {
+        Usuario user = Usuario.find("byEmail", username).first();
+        return user != null && user.clave.equals(password);
+    }
+}
+
