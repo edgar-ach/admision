c3f2b1bee69d78aa1067dd66100d00eeac569a7f
edgar-ach <chirix27@gmail.com>
2015-02-12 09:21:49 -0500

    Ruta nuevo usuario

--- app/controllers/Security.java
+++ app/controllers/Security.java
@@ -3,10 +3,17 @@ package controllers;
 import models.Usuario;
 
 public class Security extends Secure.Security {
-    
-    static boolean authenticate(String username, String password) {
-        Usuario user = Usuario.find("byEmail", username).first();
-        return user != null && user.clave.equals(password);
-    }
-}
 
+	static boolean authenticate(String username, String password) {
+		Usuario user = Usuario.find("byEmail", username).first();
+		return user != null && user.clave.equals(password);
+	}
+
+	public static void users() {
+		render();
+	}
+
+	public static void nuevo() {
+
+	}
+}
