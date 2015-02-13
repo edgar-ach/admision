d02c5df196d53ad87834a2df47991fad40851b92
Rene Quisaguano <renequisaguano@gmail.com>
2015-02-12 21:02:45 -0500

    Gestionando estudios

--- app/controllers/Estudios.java
+++ app/controllers/Estudios.java
@@ -1,12 +1,18 @@
 package controllers;
 
+import java.util.List;
+
+import models.Pais;
 import play.mvc.Controller;
 
 public class Estudios extends Controller {
+	static Pais paises=new Pais();
+	
+
 	
 	public static void index(){
-	
-		render();
+		List<String> listaPaises=paises.obtenerListado();
+		render(listaPaises);
 	}
 
 }
--- app/models/Estudio.java
+++ app/models/Estudio.java
@@ -1,10 +1,25 @@
 package models;
 
-public class Estudio {
+import javax.persistence.Entity;
+
+import play.db.jpa.Model;
+
+@Entity
+public class Estudio extends Model{
+	
 	
 	String titulo;
 	String universidad;
 	String provincia;
 	String ciudad;
+	public Estudio(String titulo, String universidad, String provincia,String ciudad) {
+		super();
+		this.titulo = titulo;
+		this.universidad = universidad;
+		this.provincia = provincia;
+		this.ciudad = ciudad;
+	}
+	
+	
 	
 }
